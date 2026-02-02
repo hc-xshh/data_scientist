ef3a37e15432143aa90306d204409fe9895e4ed1
hc-xshh <854034863@qq.com>
2026-02-02 14:20:27 +0800

    AIQW

---
+++ agents/Agent_AIQW.py
@@ -0,0 +1,13 @@
+# 问答 Agent
+# 智能问答
+
+from langchain.agents import create_agent
+from config import settings
+from models.Deepseek_Models import call_deepseek_chat
+from prompts import AIQWPrompt
+
+agent = create_agent(
+    model=call_deepseek_chat(),
+    system_prompt=AIQWPrompt
+)
+
--- prompts/__init__.py
+++ prompts/__init__.py
@@ -3,11 +3,13 @@ from .Prompts_Insighter_Reporter import Prompt as InsighterReporterPrompt
 from .Prompt_File_Analysis import Prompt as FileAnalysisPrompt
 from .Prompt_Orchestrator import Prompt as OrchestratorPrompt
 from .Prompt_HTML_Gen import Prompt as HTMLGenPrompt
+from .Prompt_AIQW import Prompt as AIQWPrompt
 
 __all__ = [
     "DataExplorerPrompts",
     "InsighterReporterPrompt",
     "FileAnalysisPrompt",
     "OrchestratorPrompt",
-    "HTMLGenPrompt"
+    "HTMLGenPrompt",
+    "AIQWPrompt",
 ]
